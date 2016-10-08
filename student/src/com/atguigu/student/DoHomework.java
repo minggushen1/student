9f6a473e6e28e10afe23baea2675a3202f298eb3
minggushen <minggushen@qq.com>
2016-10-08 19:39:56 +0800

    commit

--- student/src/com/atguigu/student/DoHomework.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.student;
-
-public class DoHomework {
-	public static void main(String[] args) {
-		System.out.println("do homework");
-	}
-}
--- student/src/com/atguigu/student/FuckHomework.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.student;
-
-public class FuckHomework {
-	public static void main(String[] args) {
-		System.out.println("fuck homework");
-	}
-}
